3a5cbd9b6525f8ea1d230f58a27b116ca4056302
Yitao Sun <yitao.sun@appdynamics.com>
2014-08-06 13:56:52 -0700

    Cleaned up InvokeGenericMethodTest

--- Tests/InvokeGenericMethodTest.cs
+++ Tests/InvokeGenericMethodTest.cs
@@ -8,9 +8,28 @@ namespace TestApp.Tests
     {
         public override void DoSomething()
         {
-            Type type = typeof(System.ServiceModel.Channels.MessageHeaders);
-            MethodInfo method = type.GetMethod("GetHeader<object>");
-            Console.WriteLine("Got method {0}", method);
+            Type type = typeof(GenericMethodTest);
+            object obj = new GenericMethodTest();
+            object[] methodParams = new object[] { 10 };
+            MethodInfo genericMethod = type.GetMethod("GetSomething");
+            MethodInfo resolvedMethod = genericMethod.MakeGenericMethod(new Type[] {typeof(int)});
+            TryInvoke(obj, genericMethod, methodParams);
+            TryInvoke(obj, resolvedMethod, methodParams);
+        }
+
+        private void TryInvoke(object obj, MethodInfo m, object[] p)
+        {
+            try
+            {
+                Console.Write("Trying to invoke {0}... ", m);
+                m.Invoke(obj, p);
+                Console.WriteLine("succeeded");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed");
+                Console.WriteLine(e);
+            }            
         }
 
     }
