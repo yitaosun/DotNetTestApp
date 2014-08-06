06ca64a9317fdb3088ecce48c818304ecd498f18
yitao.sun <yitao.sun@appdynamics.com>
2014-08-06 13:45:27 -0700

    Added a test that implements a generic method

---
+++ Tests/GenericMethodTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TestApp.Tests
+{
+    class GenericMethodTest : ATest
+    {
+        public override void DoSomething()
+        {
+            System.Threading.Thread.Sleep(500);
+        }
+
+        public T GetSomething<T>(int i)
+        {
+            Type type = typeof(T);
+            if (type == typeof(string) || type == typeof(object))
+                return (T)(object)i.ToString();
+            else if (type == typeof(int))
+                return (T)(object)i;
+            else if (type == typeof(float))
+                return (T)(object)(float)i;
+            else
+                return default(T);
+        }
+    }
+}
