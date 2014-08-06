089ac7fcec2fbb50576f29e77a3ced24bc617d4e
Yitao Sun <yitao.sun@appdynamics.com>
2014-08-06 13:46:28 -0700

    Added a test that invokes a generic method via reflection

---
+++ Tests/InvokeGenericMethodTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace TestApp.Tests
+{
+    class InvokeGenericMethodTest : ATest
+    {
+        public override void DoSomething()
+        {
+            Type type = typeof(System.ServiceModel.Channels.MessageHeaders);
+            MethodInfo method = type.GetMethod("GetHeader<object>");
+            Console.WriteLine("Got method {0}", method);
+        }
+
+    }
+}
